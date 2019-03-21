91c3558d1ed97684f08016d55ede767c08cb5587
Tyler Porter <tyler.b.porter@gmail.com>
2019-03-21 01:17:44 +0000

    Added image upload for instructors and visible course images through Amazon S3

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,9 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
+  has_many :images
+
+  mount_uploader :image, ImageUploader
 
   validates :title, presence: true
   validates :description, presence: true
