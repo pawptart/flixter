9796b66b8b7262afe1d10833c3ba8d51cb103d7b
Tyler Porter <tyler.b.porter@gmail.com>
2019-03-24 18:27:48 +0000

    Minor styling tweaks

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,7 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV["AWS_ACCESS_KEY"],
     secret_access_key: ENV["AWS_SECRET_KEY"],
-    region:            ENV["AWS_REGION"]
-  
+    region:            ENV["AWS_REGION"],
   }
 
   if Rails.env.development?
