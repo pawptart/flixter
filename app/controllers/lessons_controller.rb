e9b915ab348b4a9ca17d67a83ed521e282a307e3
Tyler Porter <tyler.b.porter@gmail.com>
2019-03-24 19:41:40 +0000

    required users to be enrolled to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course
 
   def show
+    require_enrolled_in_current_course
   end
 
   private
+
+  def require_enrolled_in_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled in the course to view this lesson!"
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
