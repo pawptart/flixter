a3ccaf2a50b9c1495e776701e38be250222b2127
Tyler Porter <tyler.b.porter@gmail.com>
2019-03-31 22:24:35 +0000

    Fixed bug with CSRF tokens

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
+  self.per_form_csrf_tokens = true
 
   def new
     @course = Course.new
